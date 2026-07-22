13eed5eba7d1f83b5ba2b0bc0610683beadf2922
YBaskoroAdi-dotcom <gilang.baskoro07@gmail.com>
2026-07-22 11:24:55 +0700

    Finalisasi konfigurasi database clever cloud

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ class Config:
     """
 
     # Konfigurasi Database MySQL
-    HOST = "localhost"
+    HOST = "bblllt163rfjoed51men-mysql.services.clever-cloud.com"
     PORT = 3306
-    USER = "root"
-    PASSWORD = ""
-    DATABASE = "db_akademik"
+    USER = "u8pwnmcoqkbsgumx"
+    PASSWORD = "qzAmSTNl0mMpCRsZOfcE"
+    DATABASE = "bblllt163rfjoed51men"
 
     # Secret Key Flask
     SECRET_KEY = "praktikum-flask-2026"
